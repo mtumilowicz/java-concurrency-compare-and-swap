9598762ca7874ab2f59899e64a1573d78bce5015
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-10-23 17:30:44 +0200

    Initial commit

---
+++ src/main/java/com/example/javacompareandswap/JavaCompareAndSwapApplication.java
@@ -0,0 +1,12 @@
+package com.example.javacompareandswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCompareAndSwapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCompareAndSwapApplication.class, args);
+    }
+}
