5cc520f81188e854b67b2a219684a9ac0dc71511
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-10-24 20:32:33 +0200

    swagger

---
+++ src/main/java/com/example/javacompareandswap/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.example.javacompareandswap;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by mtumilowicz on 2018-10-24.
+ */
+@Configuration
+@EnableSwagger2
+@AllArgsConstructor
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
