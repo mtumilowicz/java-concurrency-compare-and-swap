fa3fd1dd87979361bcbef85e2ac2ce43f10f3fec
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-10-25 18:44:33 +0200

    fibonacci update

--- src/main/java/com/example/javacompareandswap/Fibonacci.java
+++ src/main/java/com/example/javacompareandswap/Fibonacci.java
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Created by mtumilowicz on 2018-10-24.
  */
 public final class Fibonacci {
-    private AtomicInteger first = new AtomicInteger(1);
+    private AtomicInteger first = new AtomicInteger(0);
     private AtomicInteger second = new AtomicInteger(1);
     
     public synchronized int next() {
